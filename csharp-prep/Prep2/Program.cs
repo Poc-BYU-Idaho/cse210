1c0a4045376cf17e9d2d124ff371ad69ee6636d9
Poc-BYU-Idaho <poc22002@byui.edu>
2023-09-14 09:58:43 -0600

    Completed prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letterGrade = "";
+        Console.Write("What percent do you have in the class? ");
+        string gradePercent = Console.ReadLine();
+        float useableGradePercent = float.Parse(gradePercent);
+        if (useableGradePercent >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (useableGradePercent >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (useableGradePercent >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (useableGradePercent >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+        Console.WriteLine($"Your grade is: {letterGrade}");
+        if (useableGradePercent > 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass the class, but you can get it next time!");
+        }
     }
 }
