82a83c597eb3cef62a12f341d3e26b335ef4c6a9
Poc-BYU-Idaho <poc22002@byui.edu>
2023-09-19 20:04:03 -0600

    Completed prep 3-5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess = -1;
+
+        // Console.Write("What is the magic number? ");
+        // string userNumber = Console.ReadLine();
+        // int magicNumber = int.Parse(userNumber);
+
+        Random rng = new Random();
+        string keepPlaying = "yes";
+
+        while (keepPlaying =="yes")
+        {
+            int guessCount = 0;
+            int magicNumber = rng.Next(1, 101);
+
+            while (guess != magicNumber)
+            {
+                guessCount += 1;
+                Console.Write("What is your guess? ");
+                string userGuess = Console.ReadLine();
+                guess = int.Parse(userGuess);
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.Write($"You guessed it!\nGuess Count: {guessCount}\nWould you like to keep playing? (yes/no) ");
+                    keepPlaying = Console.ReadLine();
+                }
+            }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int convertedInput = -1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (convertedInput != 0)
+        {
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            convertedInput = int.Parse(userInput);
+            if (convertedInput != 0)
+            {
+                numbers.Add(convertedInput);
+            }
+        }
+        numbers.Sort();
+        float numberSum = 0;
+        int greatestNumber = numbers.Last();
+        int smallestPositive = -1;
+        foreach (int number in numbers)
+        {
+            numberSum += number;
+            if (number > 0 && number < smallestPositive)
+            {
+                smallestPositive = number;
+            }
+        }
+        Console.WriteLine($"The sum is: {numberSum}\nThe average is: {numberSum / numbers.Count}\nThe largest number is: {greatestNumber}\nThe smallest positive number is: {smallestPositive}");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
