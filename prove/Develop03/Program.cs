cd996e3411f731aa63df1a6dfcc12a23462d30ed
Poc-BYU-Idaho <poc22002@byui.edu>
2023-10-13 12:40:11 -0600

    Removed word from scripture that was used for a test

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Reference reference = new Reference("1 Nephi", 1, 1, 2);
-        Scripture scripture = new Scripture(reference, "I, Nephi, having been born of goodly parents, therefore I was taught somewhat in all the learning of my father; and having seen many afflictions in the course of my days, nevertheless, having been highly favored of the Lord in all my days; yea, having had a great knowledge of the goodness and the mysteries of God, therefore I make a record of my proceedings in my days. Yea, I make a record in the language of my father, which consists of the learning of the Jews and the language of the Egyptians. YAAAAAA");
+        Scripture scripture = new Scripture(reference, "I, Nephi, having been born of goodly parents, therefore I was taught somewhat in all the learning of my father; and having seen many afflictions in the course of my days, nevertheless, having been highly favored of the Lord in all my days; yea, having had a great knowledge of the goodness and the mysteries of God, therefore I make a record of my proceedings in my days. Yea, I make a record in the language of my father, which consists of the learning of the Jews and the language of the Egyptians.");
 
         string userInput = "";
         Console.Clear();
