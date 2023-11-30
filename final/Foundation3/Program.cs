c287ea6795b6ba666885b792963e43f16f5b0b85
Poc-BYU-Idaho <poc22002@byui.edu>
2023-11-30 15:42:41 -0700

    Shortened foundation 3 program

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,19 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Reception reception = new Reception("rsvpemail@gmail.com", "title", "a description", "12/10/2023", "5PM", new Address("test address", "test city", "test state", "test country"));
-        reception.GetDetails();
-        reception.GetFullDetails();
-        reception.GetShortDetails();
+        List<Event> events = new List<Event>()
+        {
+            new Reception("rsvpemail@gmail.com", "title", "a description", "12/10/2023", "5PM", new Address("test address", "test city", "test state", "test country")),
+            new OutdoorGathering("It will be sunny.", "title", "a description", "12/10/2023", "5PM", new Address("test address", "test city", "test state", "test country")),
+            new Lecture("A speaker", 10, "title", "a description", "12/10/2023", "5PM", new Address("test address", "test city", "test state", "test country"))
+        };
 
-        OutdoorGathering outdoorGathering = new OutdoorGathering("It will be sunny.", "title", "a description", "12/10/2023", "5PM", new Address("test address", "test city", "test state", "test country"));
-        outdoorGathering.GetDetails();
-        outdoorGathering.GetFullDetails();
-        outdoorGathering.GetShortDetails();
-
-        Lecture lecture = new Lecture("A speaker", 10, "title", "a description", "12/10/2023", "5PM", new Address("test address", "test city", "test state", "test country"));
-        lecture.GetDetails();
-        lecture.GetFullDetails();
-        lecture.GetShortDetails();
+        foreach (Event eventObject in events)
+        {
+                eventObject.GetDetails();
+                eventObject.GetFullDetails();
+                eventObject.GetShortDetails();
+        }
     }
 }
