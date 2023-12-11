c9170ece16e96b403a7ae547818ff232419c8616
Poc-BYU-Idaho <poc22002@byui.edu>
2023-12-11 09:36:11 -0700

    Further improved foundation 4 list formatting

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -6,21 +6,27 @@ class Program
     {
         foreach (Video video in new List<Video>()
         {
-            new Video("Cool Video", "Bob", 20, new List<Comment>(){
-            new Comment("Joe", "This is my comment."),
-            new Comment("Jake", "Good"),
-            new Comment("James", "Bad")}),
+            new Video("Cool Video", "Bob", 20, new List<Comment>()
+            {
+                new Comment("Joe", "This is my comment."),
+                new Comment("Jake", "Good"),
+                new Comment("James", "Bad")
+            }),
 
-            new Video("Cooler Video", "Bill", 500, new List<Comment>(){
-            new Comment("Chad", "Cool"),
-            new Comment("Carson", "Boring"),
-            new Comment("Charlie", "Great")}),
+            new Video("Cooler Video", "Bill", 500, new List<Comment>()
+            {
+                new Comment("Chad", "Cool"),
+                new Comment("Carson", "Boring"),
+                new Comment("Charlie", "Great")
+            }),
 
-            new Video("Coolest Video", "Benson", 1260, new List<Comment>(){
-            new Comment("Roy", "Nice video"),
-            new Comment("Ray", "Could be better"),
-            new Comment("Ryan", "Video is too long"),
-            new Comment("Rigby", "I am the fourth commentor")})
+            new Video("Coolest Video", "Benson", 1260, new List<Comment>()
+            {
+                new Comment("Roy", "Nice video"),
+                new Comment("Ray", "Could be better"),
+                new Comment("Ryan", "Video is too long"),
+                new Comment("Rigby", "I am the fourth commentor")
+            })
         })
         {
             video.Display();
