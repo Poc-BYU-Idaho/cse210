3373367f8920c7f707f387e9e7e4b8758e5aa275
Poc-BYU-Idaho <poc22002@byui.edu>
2023-12-11 09:30:11 -0700

    Improved list formatting in foundation 4 projects

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,7 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        foreach (Event eventObject in new List<Event>(){new Reception("rsvpemail@gmail.com", "title", "a description", "12/10/2023", "5PM", new Address("test address", "test city", "test state", "test country")), new OutdoorGathering("It will be sunny.", "title", "a description", "12/10/2023", "5PM", new Address("test address", "test city", "test state", "test country")), new Lecture("A speaker", 10, "title", "a description", "12/10/2023", "5PM", new Address("test address", "test city", "test state", "test country"))})
+        foreach (Event eventObject in new List<Event>()
+        {
+            new Reception("rsvpemail@gmail.com", "title", "a description", "12/10/2023", "5PM", new Address("test address", "test city", "test state", "test country")),
+            new OutdoorGathering("It will be sunny.", "title", "a description", "12/10/2023", "5PM", new Address("test address", "test city", "test state", "test country")),
+            new Lecture("A speaker", 10, "title", "a description", "12/10/2023", "5PM", new Address("test address", "test city", "test state", "test country"))
+        })
         {
                 eventObject.GetDetails();
                 Console.WriteLine();
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,7 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        foreach (Activity activity in new List<Activity>(){new Running(4.8, "03 Nov 2022", 30), new Cycling(15, "03 Nov 2022", 45), new Swimming(16, "03 Nov 2022", 10)})
+        foreach (Activity activity in new List<Activity>()
+        {
+            new Running(4.8, "03 Nov 2022", 30),
+            new Cycling(15, "03 Nov 2022", 45),
+            new Swimming(16, "03 Nov 2022", 10)
+        })
         {
             Console.WriteLine(activity.GetSummary());
         }
